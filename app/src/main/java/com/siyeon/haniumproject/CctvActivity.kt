da3f256851b69f3480ae0f7adc356325267ae23b
2yeram <ramramm_@naver.com>
2023-09-09 14:18:42 +0900

    [CCTV] webview 링크 두 개끼리 번갈아가며 전환 가능

--- app/src/main/java/com/siyeon/haniumproject/CctvActivity.kt
+++ app/src/main/java/com/siyeon/haniumproject/CctvActivity.kt
@@ -4,27 +4,27 @@ import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class CctvActivity: AppCompatActivity() {
 
+    private lateinit var webView: WebView
+    private var isInitialUrl = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cctv_layout)
 
-        // java activity 파일 삭제하고 입력한 코드
-        val webView = findViewById<WebView>(R.id.webView)
+        webView = findViewById<WebView>(R.id.webView)
         webView.apply {
             webViewClient = WebViewClient()
             webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true
         }
 
-        webView.loadUrl("https://www.youtube.com/watch?v=6Dbq05hchX4")
-        //여기까지
-
+        loadInitialUrl()
 
         title = "CCTV 확인"
 
@@ -32,8 +32,27 @@ class CctvActivity: AppCompatActivity() {
         buttonReturn.setOnClickListener {
             finish()
         }
+
+        val buttonTurn = findViewById<Button>(R.id.bt_turn)
+        buttonTurn.setOnClickListener {
+            // Toggle between InitialUrl and newUrl
+            if (isInitialUrl) {
+                loadNewUrl("https://www.youtube.com/watch?v=3sL2hceTCU8&list=LL&index=9")
+            } else {
+                loadInitialUrl()
+            }
+            // Toggle the flag
+            isInitialUrl = !isInitialUrl
+        }
     }
 
+    private fun loadInitialUrl() {
+        // Load the Initial URL
+        webView.loadUrl("https://www.youtube.com/watch?v=6Dbq05hchX4")
+    }
 
-
+    private fun loadNewUrl(url: String) {
+        // Load a new URL into the WebView
+        webView.loadUrl(url)
+    }
 }
